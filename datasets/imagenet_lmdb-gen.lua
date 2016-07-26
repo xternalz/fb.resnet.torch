a63a6a8366551bbd382a797f26606ac3e50ec92a
Jason Kuen <xternalz@users.noreply.github.com>
2016-07-26 12:57:39 +0800

    Change backend to lightningmdb

--- datasets/imagenet_lmdb-gen.lua
+++ datasets/imagenet_lmdb-gen.lua
@@ -15,25 +15,60 @@
 
 local M = {}
 
+local function all_keys(cursor_,key_,op_)
+   return coroutine.wrap(
+      function()
+         local k = key_
+         local v
+         repeat
+            k,v = cursor_:get(k,op_ or MDB.NEXT)
+            if k then
+              coroutine.yield(k,v)
+            end
+         until (not k)
+      end)
+end
+
 local function getKeys(path, split)
-   local Keys
+   local lightningmdb = require 'lightningmdb'
+   env = lightningmdb.env_create()
+   local LMDB_MAP_SIZE = 1099511627776 -- 1 TB
+   env:set_mapsize(LMDB_MAP_SIZE)
+   local flags = lightningmdb.MDB_RDONLY + lightningmdb.MDB_NOTLS
+   local db, err = env:open(path, flags, 0664)
+   if not db then
+     -- unable to open Database => this might be due to a permission error on
+     -- the lock file so we will try again with MDB_NOLOCK. MDB_NOLOCK is safe
+     -- in this process as we are opening the database in read-only mode.
+     -- However if another process is writing into the database we might have a
+     -- concurrency issue - note that this shouldn't happen in DIGITS since the
+     -- database is written only once during dataset creation
+     print('opening LMDB database failed with error: "' .. err .. '". Trying with MDB_NOLOCK')
+     flags = bit.bor(flags, lighningmdb.MDB_NOLOCK)
+     -- we need to close/re-open the LMDB environment
+     env:close()
+     env = lightningmdb.env_create()
+     env:set_mapsize(LMDB_MAP_SIZE)
+     db, err = env:open(path, flags, 0664)
+     if not db then
+         error('opening LMDB database failed with error: ' .. err)
+     end
+   end
+   local total = env:stat().ms_entries
+   local txn = env:txn_begin(nil, lightningmdb.MDB_RDONLY)
+   local d = txn:dbi_open(nil,0)
+   local cursor = txn:cursor_open(d)
    local tds = require 'tds'
-   Keys = tds.Vec()
-   local lmdb = require 'lmdb'
-   local db = lmdb.env{Path = path, Name = split}
-   db:open()
-   local txn = db:txn(true)
-   local cursor = txn:cursor()
-   local numEntries = db:stat().entries
-   for i = 1, numEntries do
-      local key = select(1, cursor:get())
-      Keys[i] = key
-      if i < numEntries then
-         cursor:next()
-      end
+   local Keys = tds.Vec()
+   local i = 0
+   for k,v in all_keys(cursor,nil,lightningmdb.MDB_NEXT) do
+     i=i+1
+     Keys[i] = k
    end
    cursor:close()
-   db:close()
+   env:dbi_close(d)
+   txn:abort()
+   env:close()
    return Keys
 end
 
