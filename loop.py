3d6228691c43cec5576c289523ca115e63eded59
XterNalz <jason7fd@gmail.com>
2016-09-04 21:50:39 +0800

    evaluate

--- loop.py
+++ loop.py
@@ -3,6 +3,7 @@ import subprocess
 f = open('run_log.txt','a+')
 while True:
     try:
+        subprocess.call(["pkill", "luajit", "-9"])
         output = subprocess.check_output(['th', 'main.lua'])
         f.write('\n')
         f.write(output)
